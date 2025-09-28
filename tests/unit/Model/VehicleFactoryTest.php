eb6b1a7a3207e6be187bebd82b573eaa55ff2306
Jorge Cortés <jcortesanguita@gmail.com>
2025-09-28 18:41:47 +0200

    Add unit test for factory

--- tests/unit/Model/VehicleFactoryTest.php
+++ tests/unit/Model/VehicleFactoryTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Tests\unit\Model;
 
+use App\Model\Car;
+use App\Model\Location;
+use App\Model\Motorbike;
+use App\Model\VehicleFactory;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,16 +17,47 @@ final class VehicleFactoryTest extends TestCase
 {
     public function testWhenCreateVehicleOnCarTypeThenReturnsCar(): void
     {
-        self::markTestIncomplete('To be implemented');
+        $factory = new VehicleFactory();
+        $location = new Location('Los Angeles', 'CA');
+        $data = [
+            'type' => 'car',
+            'name' => 'Toyota Camry',
+            'doors' => 4,
+            'fuel' => 'gasoline',
+        ];
+
+        $vehicle = $factory->createVehicle($data, $location);
+
+        self::assertInstanceOf(Car::class, $vehicle);
     }
 
     public function testWhenCreateVehicleOnMotorbikeTypeThenReturnsMotorbike(): void
     {
-        self::markTestIncomplete('To be implemented');
+        $factory = new VehicleFactory();
+        $location = new Location('Los Angeles', 'CA');
+        $data = [
+            'type' => 'motorbike',
+            'name' => 'Harley Davidson',
+            'cc' => 750,
+            'has_trunk' => true,
+        ];
+
+        $vehicle = $factory->createVehicle($data, $location);
+
+        self::assertInstanceOf(Motorbike::class, $vehicle);
     }
 
     public function testWhenCreateVehicleOnUnknownTypeThenThrowsInvalidArgumentException(): void
     {
-        self::markTestIncomplete('To be implemented');
+        $factory = new VehicleFactory();
+        $location = new Location('New New York', 'Mars');
+        $data = [
+            'type' => 'UFO',
+            'name' => 'flying saucer',
+        ];
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $factory->createVehicle($data, $location);
     }
 }
